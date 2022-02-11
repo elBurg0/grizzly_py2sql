23507c6397e037841a7921ebe50f4f4a5bdb7598
Leon - Mac <burger.leon@web.de>
2022-02-11 06:35:53 +0100

    created use cases

---
+++ use_cases.py
@@ -0,0 +1,18 @@
+# load connection
+con = ...
+
+# load table
+table = Table(con, "table")
+table = grizzly.read_table("events")
+
+
+def udf():
+    if table.column > 10
+    table.column = 12
+
+UPDATE table
+SET table.column = 12
+WHERE table.column > 12
+
+
+# Bei return auch SQL-Return
