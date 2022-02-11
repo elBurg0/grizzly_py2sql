061cf345fa4c835a417f4f9cbc7c6917ec2f43cb
Leon - Mac <burger.leon@web.de>
2022-02-11 19:38:25 +0100

    edited uses cases

--- use_cases.py
+++ use_cases.py
@@ -1,4 +1,7 @@
 # load connection
+from turtle import begin_fill
+
+
 con = ...
 
 # load table
@@ -7,12 +10,31 @@ table = grizzly.read_table("events")
 
 
 def udf():
-    if table.column > 10
-    table.column = 12
+    if table.column > 10:
+        table.column = 12
 
 UPDATE table
 SET table.column = 12
 WHERE table.column > 12
 
 
-# Bei return auch SQL-Return
+
+def udf():
+    if table.column > 10:
+        print(table.column)
+
+SELECT table.column
+FROM table
+WHERE table.column > 10
+
+BEGIN 
+    for r in (SELECT * FROM table WHERE table.column > 10)
+    loop
+        dbms_output.put_line(r.column);
+    end loop;
+END;
+
+
+# Bei return auch SQL-Return, aber was? -> Liste, cursor, df
+
+def udf():
