d9fad6dcc1f05d8cd6068cf41335ceb0bb739ba6
Leon - Win <burger.leon@web.de>
2022-03-19 20:50:50 +0100

    changed time format

--- timing.py
+++ timing.py
@@ -5,6 +5,7 @@ def timing(f):
         time1 = time.time()
         ret = f(*args, **kwargs)
         time2 = time.time()
-        print(f'--- TIME: {f.__name__} function took {(time2-time1)*1000.0:.0f} ms')
-        return ret
+        time_clean = int(f"{(time2-time1)*1000.0:.0f}")
+        print(f'--- TIME: {f.__name__} function took {time_clean} ms')
+        return ret, time_clean
     return wrap
