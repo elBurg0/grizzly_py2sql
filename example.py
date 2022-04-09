d44a3b51e1976e2dc9b6d6c688d1dcf6d23a94ec
Leon - Win <burger.leon@web.de>
2022-04-09 22:43:40 +0200

    moved choice of connection and function

--- example.py
+++ example.py
@@ -1,31 +1,33 @@
 import grizzly
 import grizzly.aggregates
-
 from grizzly.relationaldbexecutor import RelationalExecutor
 from grizzly.sqlgenerator import SQLGenerator
 
 import functions
+import config
 
-import sqlite3
 import psycopg2
 import cx_Oracle
 
-# SQLite db
-#con = sqlite3.connect("grizzly.db")
-
-# Oracle pluggable db
-#con = cx_Oracle.connect(user='demopython', password='orcl_py123', dsn='127.0.0.1/orclpdb')
+# Connection to test
+conf = config.oracle
+# Function to compile and execute
+func = functions.myfunc4
 
 # Oracle db
-con = cx_Oracle.connect(user='demopython', password='orcl2_py123', dsn='orcl2')
-
-#Postgresql db
-#con = psycopg2.connect(dbname="postgres", user="post_py", password="post_py_DB123")
+if conf == config.oracle:
+    con = cx_Oracle.connect(user=conf['user'], password=conf['password'], dsn=conf['dsn'])
+# Postgres local db
+elif conf == config.postgres:
+    con = psycopg2.connect(dbname=conf['dbname'], user=conf['user'], password=conf['password'])
+# Postgres server db
+elif conf == config.postgres_server:
+    con = psycopg2.connect(dbname=conf['dbname'], user=conf['user'], password=conf['password'], host=conf['host'])
 
 
 grizzly.use(RelationalExecutor(con, SQLGenerator('oracle')))
 df = grizzly.read_table("speedtest")
 df = df[["test_id", "test_text", "test_float", "test_number"]]
-df["udf"] = df["test_id"].map(functions.myfunc3)
+df["udf"] = df["test_id"].map(func)
 print(df.generateQuery())
 df.show(pretty=True)
