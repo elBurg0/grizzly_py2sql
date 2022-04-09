3a4c4a8e0ea9d8b6bef66e6f7e64d75e787a880a
Leon - Win <burger.leon@web.de>
2022-04-09 21:33:17 +0200

    added separate file for test functions

---
+++ functions.py
@@ -0,0 +1,48 @@
+import grizzly
+
+def myfunc1(a: int) -> int:
+    return a + 3
+
+
+def myfunc2(a: int) -> str:
+    i:float = a + 2.31
+    return "g_" + i
+
+def myfunc3(a: int) -> int:
+    i: int = 22
+    m: int = a + i
+    for i in range(1, a):
+        if i + a > 20:
+            return 20 * a
+        else:
+            return 0
+            
+    return m
+
+def myfunc4(a: int) -> str:
+    c = "g_"
+    n = 2.1
+    f = 0
+    g_df1 = grizzly.read_table("speedtest")
+    g_df1.generateQuery()
+    for row in g_df1:
+        f = f + a + row.test_id
+
+    t: float = f + n
+    return c + t
+
+def myfunc5(a: int) -> str:
+    c = "g_"
+    n = 2.1
+    f = 0
+    g_df1 = grizzly.read_table("speedtest")
+    g_df1.generateQuery()
+    for row in g_df1:
+        if row.test_id >= a -2:
+            if row.test_id <= a +2:
+                if row.test_number > 40:
+                    f = f + row.test_number
+
+
+    t: float = f + n
+    return f
