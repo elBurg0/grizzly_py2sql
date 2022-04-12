242bbfb291f09cd4a819f1366d49b30c57977210
Leon - Win <burger.leon@web.de>
2022-04-12 17:31:24 +0200

    renamed file

--- test_functions.py
+++ test_functions.py
@@ -18,7 +18,7 @@ def myfunc3(a: int) -> int:
             
     return m
 
-def myfunc4(a: int) -> str:
+def func4(a: int) -> str:
     c = "g_"
     n = 2.1
     f = 0
@@ -36,10 +36,15 @@ def myfunc5(a: int) -> str:
     f = 0
     g_df1 = grizzly.read_table("speedtest")
     g_df1.generateQuery()
+    #g_df1[g_df1["test_number"] > 20]
     for row in g_df1:
         if row.test_id >= a -2:
             if row.test_id <= a +2:
                 if row.test_number > 40:
                     f = f + row.test_number
                     
+                
+            
+        
+    
     return f
