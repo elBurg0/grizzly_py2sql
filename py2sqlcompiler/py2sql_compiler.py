38bf8ac3b1020e4f4d757d5f35cc79868a7359bc
Leon - Win <burger.leon@web.de>
2022-04-12 20:21:06 +0200

    configuration for new parser

--- py2sqlcompiler/py2sql_compiler.py
+++ py2sqlcompiler/py2sql_compiler.py
@@ -1,9 +1,11 @@
 import pathlib
 from antlr4 import *
-from py2sqlcompiler.py_parser5.Python3Lexer import Python3Lexer
-from py2sqlcompiler.py_parser5.Python3Parser import Python3Parser
-from py2sqlcompiler.py_parser5.Python3Listener import Python3Listener
-from py2sqlcompiler.py_parser5.Python3Visitor import Python3Visitor
+from py2sqlcompiler.py_parser_024.Python3dLexer import Python3dLexer
+from py2sqlcompiler.py_parser_024.Python3dParser import Python3dParser
+from py2sqlcompiler.py_parser_024.Python3dListener import Python3dListener
+from py2sqlcompiler.py_parser_024.Python3dVisitor import Python3dVisitor
+
+import grizzly
 
 
 def main(argv, templates):
@@ -12,16 +14,16 @@ def main(argv, templates):
     else:
         input_stream = InputStream(argv[1])
 
-    lexer = Python3Lexer(input_stream)
+    lexer = Python3dLexer(input_stream)
     stream = CommonTokenStream(lexer)
-    parser = Python3Parser(stream)
+    parser = Python3dParser(stream)
     tree = parser.file_input()
 
-    listener = Python3Listener()
+    listener = Python3dListener()
     walker = ParseTreeWalker()
     walker.walk(listener, tree)
 
-    visitor = Python3Visitor(templates)
+    visitor = Python3dVisitor(templates)
     visitor.visit(tree)
 
     # Add statements that should be queried before PL/SQL Block
--- py2sqlcompiler/test_tokens_code.py
+++ py2sqlcompiler/test_tokens_code.py
@@ -1,14 +1,12 @@
-    c = "g_"
-    n = 2.1
-    f = 0
-    g_df1 = grizzly.read_table("speedtest")
-    g_df1.generateQuery()
-    for row in g_df1:
-        if row.test_id >= a -2:
-            if row.test_id <= a +2:
-                f = f + row.test_id
-                
+c = "g_"
+n = 2.1
+f = 0
+g_df1 = grizzly.read_table("speedtest")
+g_df1[g_df1["test_number"] > 20]
+for row in g_df1:
+    if row.test_id >= a -2:
+        if row.test_id <= a +2:
+            if row.test_number > 40:
+                f = f + row.test_number
 
-        
-    t: float = f + n
-    return f
+return f
