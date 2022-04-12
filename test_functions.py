1af268f6a51cf521177fc0805ad91cd27c0ba83b
Leon - Win <burger.leon@web.de>
2022-04-12 20:22:03 +0200

    added new functionalty test

--- test_functions.py
+++ test_functions.py
@@ -35,16 +35,12 @@ def myfunc5(a: int) -> str:
     n = 2.1
     f = 0
     g_df1 = grizzly.read_table("speedtest")
-    g_df1.generateQuery()
-    #g_df1[g_df1["test_number"] > 20]
+    
+    g_df1 = g_df1[g_df1.test_number > 20] 
+
     for row in g_df1:
         if row.test_id >= a -2:
             if row.test_id <= a +2:
                 if row.test_number > 40:
                     f = f + row.test_number
-                    
-                
-            
-        
-    
     return f
