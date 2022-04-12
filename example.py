5e2d87143d4acb9b712f8540fc5ff9325711bc52
Leon - Win <burger.leon@web.de>
2022-04-12 20:19:18 +0200

    added test_funtions import

--- example.py
+++ example.py
@@ -12,20 +12,23 @@ import cx_Oracle
 # Connection to test
 conf = config.oracle
 # Function to compile and execute
-func = functions.myfunc4
+func = functions.myfunc5
 
 # Oracle db
 if conf == config.oracle:
+    db = 'oracle'
     con = cx_Oracle.connect(user=conf['user'], password=conf['password'], dsn=conf['dsn'])
 # Postgres local db
 elif conf == config.postgres:
+    db = 'postgresql'
     con = psycopg2.connect(dbname=conf['dbname'], user=conf['user'], password=conf['password'])
 # Postgres server db
 elif conf == config.postgres_server:
+    db = 'postgresql'
     con = psycopg2.connect(dbname=conf['dbname'], user=conf['user'], password=conf['password'], host=conf['host'])
 
 
-grizzly.use(RelationalExecutor(con, SQLGenerator('oracle')))
+grizzly.use(RelationalExecutor(con, SQLGenerator(db)))
 df = grizzly.read_table("speedtest")
 df = df[["test_id", "test_text", "test_float", "test_number"]]
 df["udf"] = df["test_id"].map(func)
