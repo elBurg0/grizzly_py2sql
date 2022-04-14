5f603cbe348e41831d403f69fdcf644eababb26b
Leon - Win <burger.leon@web.de>
2022-04-15 00:18:09 +0200

    changed func names

--- test_functions.py
+++ test_functions.py
@@ -1,13 +1,13 @@
 import grizzly
 
-def myfunc1(a: int) -> int:
+def udf1(a: int) -> int:
     return a + 3
 
-def myfunc2(a: int) -> str:
+def udf2(a: int) -> str:
     i:float = a + 2.31
     return "g_" + i
 
-def myfunc3(a: int) -> int:
+def udf3(a: int) -> int:
     i: int = 22
     m: int = a + i
     for i in range(1, a):
@@ -17,7 +17,7 @@ def myfunc3(a: int) -> int:
             return 0 
     return m
 
-def func4(a: int) -> str:
+def udf4(a: int) -> str:
     c = "g_"
     n = 2.1
     f = 0
@@ -28,13 +28,12 @@ def func4(a: int) -> str:
     t: float = f + n
     return c + t
 
-def myfunc5(a: int) -> str:
-    c = "g_"
-    n = 2.1
+def udf5(a: int) -> str:
     f = 0
     # Kommentar
     g_df1 = grizzly.read_table("speedtest")
-    g_df1 = g_df1[g_df1.test_float > 20] 
+    g_df1 = g_df1[g_df1.test_id == a] 
+
 
     for row in g_df1:
         if row.test_id >= a -2:
@@ -43,11 +42,8 @@ def myfunc5(a: int) -> str:
                     f = f + row.test_number
     return f
 
-def myfunc6(a: int) -> str:
-    c = "g_"
-    n = 2.1
+def udf6(a: int) -> str:
     f = 0
-    # Kommentar
     g_df1 = grizzly.read_table("speedtest")
     g_df1 = g_df1[g_df1.test_float > 10] 
 
