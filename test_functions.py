eaf2d3befe914435ca0173d81deb2a676e5f0c1a
Leon - Win <burger.leon@web.de>
2022-04-29 15:52:38 +0200

    added new test functions

--- test_functions.py
+++ test_functions.py
@@ -1,11 +1,12 @@
+from opcode import haslocal
 import grizzly
 
 def udf1(a: int) -> int:
     return a + 3
 
 def udf2(a: int) -> str:
-    i:float = a + 2.31
-    return "g_" + i
+    i:float = 2.31
+    return "g_" + i + a
 
 def udf3(a: int) -> int:
     i: int = 22
@@ -32,7 +33,7 @@ def udf5(a: int) -> str:
     f = 0
     # Kommentar
     g_df1 = grizzly.read_table("speedtest")
-    g_df1 = g_df1[g_df1.test_id == a] 
+    g_df1 = g_df1[g_df1.test_id == 5] 
 
 
     for row in g_df1:
@@ -54,4 +55,44 @@ def udf6(a: int) -> str:
             break
         if tuple.test_number > 20:
             f = f + tuple.test_number
+    return f
+
+def udf7(a: int) -> str:
+    t = 1
+    while t < a:
+        t = t * 2
+    return t
+
+def udf8(a: int, b: float) -> float:
+    t = "hi"
+    try:
+        t = a / 0
+    except:
+        return 100
+    return t
+
+def udf9(a: int, b: float) -> float:
+    t = "hi"
+    i = 0
+    m = 1.4
+    
+    try:
+        if a > b:
+            return 2
+    except:
+        return 100
+    return m
+
+def udf10(a: int, b: float) -> float:
+    i = [2,3]
+    b = i[0]
+    try:
+        if a > b:
+            raise
+    except:
+        return 5
+    return b
+
+def udf11(a: int, b: float) -> float:
+    f: float = udf9(5, 2.3)
     return f
